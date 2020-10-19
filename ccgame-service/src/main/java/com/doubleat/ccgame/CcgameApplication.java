e1b52b42049b4a53a216853ddde26efc90fc6a07
Puskin2911 <nguyenthehop2000@gmail.com>
2020-10-19 16:58:17 +0700

    Init project

---
+++ ccgame-service/src/main/java/com/doubleat/ccgame/CcgameApplication.java
@@ -0,0 +1,13 @@
+package com.doubleat.ccgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcgameApplication.class, args);
+    }
+
+}
---
+++ ccgame-service/src/test/java/com/doubleat/ccgame/CcgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doubleat.ccgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
