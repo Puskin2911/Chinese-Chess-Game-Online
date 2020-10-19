cd9f7e4a87d6c1e71d71b4ae2a36e10a5672d2e3
Puskin2911 <nguyenthehop2000@gmail.com>
2020-10-19 17:32:03 +0700

    Swagger2 config

---
+++ ccgame-service/src/main/java/config/Swagger2Config.java
@@ -0,0 +1,35 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.doubleat.ccgame"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiEndPointInfo());
+    }
+
+    private ApiInfo apiEndPointInfo(){
+        return new ApiInfoBuilder()
+                .title("Chinese Chess Game API")
+                .description("Chinese Chess Game online with WebSocket")
+                .contact(new Contact("@@", "http://doubleat.com/ccgames", "hulk@gmail.com"))
+                .version("1.0.0")
+                .build();
+    }
+}
