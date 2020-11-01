b672360b66f9195f4760401ee92ff5775dbc5dab
Puskin2911 <nguyenthehop2000@gmail.com>
2020-11-01 16:20:34 +0700

    Add diagonMove and delete horseMove not use

--- ccgame-service/src/main/java/com/doubleat/ccgame/game/utils/MoveUtils.java
+++ ccgame-service/src/main/java/com/doubleat/ccgame/game/utils/MoveUtils.java
@@ -8,12 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class MoveUtils {
+
     private static final Logger logger = LoggerFactory.getLogger(MoveUtils.class);
 
     private MoveUtils() {
     }
 
-    private static void init(Board board, Position from, Position to) {
+    private static void preCheck(Board board, Position from, Position to) {
         Piece[][] pieces = board.getPieces();
         Piece current = pieces[from.getX()][from.getY()];
         if (current == null) {
@@ -23,23 +24,24 @@ public final class MoveUtils {
     }
 
     public static boolean isStraightMove(Board board, Position from, Position to) {
-        init(board, from, to);
+        preCheck(board, from, to);
 
         return from.getY() == to.getY() && from.getX() != to.getX();
     }
 
     public static boolean isSideMove(Board board, Position from, Position to) {
-        init(board, from, to);
+        preCheck(board, from, to);
 
         return from.getX() == to.getX() && from.getY() != to.getY();
     }
 
     public static boolean isDiagonalMove(Board board, Position from, Position to) {
-        return false;
-    }
+        preCheck(board, from, to);
 
-    public static boolean isHorseMove(Board board, Position from, Position to) {
-        return false;
+        int xRange = Math.abs(from.getX() - to.getX());
+        int yRange = Math.abs(from.getY() - to.getY());
+
+        return (xRange == yRange && xRange != 0);
     }
 
 }
