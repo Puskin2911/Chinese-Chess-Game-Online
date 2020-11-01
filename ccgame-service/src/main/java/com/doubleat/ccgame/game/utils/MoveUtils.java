f5cae711a2bf8c8ad1a5085b350cee454fbb2400
Puskin2911 <nguyenthehop2000@gmail.com>
2020-11-01 19:58:13 +0700

    Check valid move

--- ccgame-service/src/main/java/com/doubleat/ccgame/game/utils/MoveUtils.java
+++ ccgame-service/src/main/java/com/doubleat/ccgame/game/utils/MoveUtils.java
@@ -3,6 +3,9 @@ package com.doubleat.ccgame.game.utils;
 import com.doubleat.ccgame.game.Board;
 import com.doubleat.ccgame.game.Position;
 import com.doubleat.ccgame.game.piece.Piece;
+import org.springframework.util.StringUtils;
+
+import java.util.regex.Pattern;
 
 public final class MoveUtils {
 
@@ -44,6 +47,22 @@ public final class MoveUtils {
         int yRange = Math.abs(from.getY() - to.getY());
 
         return (xRange == yRange && xRange != 0);
+    }
+
+    /**
+     * Check if move is valid.
+     *
+     * @param move move need to check.
+     * @return {@code true} if the move is valid, other wise return {@code false}.
+     */
+    public static boolean isValidMove(String move) {
+        if (move != null && move.length() == 5) {
+            Pattern pattern = Pattern.compile("[0-9][0-8]_[0-9][0-8]");
+
+            return pattern.matcher(move).matches();
+        }
+
+        return false;
     }
 
 }
