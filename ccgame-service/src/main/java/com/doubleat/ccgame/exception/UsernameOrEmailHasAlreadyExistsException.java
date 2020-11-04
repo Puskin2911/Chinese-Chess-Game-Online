0e6e2d94127df8411018e97fb4847383c869713b
Puskin2911 <nguyenthehop2000@gmail.com>
2020-11-04 14:30:19 +0700

    refactor name and remove private field unnecessary

---
+++ ccgame-service/src/main/java/com/doubleat/ccgame/exception/UsernameOrEmailHasAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.doubleat.ccgame.exception;
+
+import lombok.Getter;
+
+@Getter
+public class UsernameOrEmailHasAlreadyExistsException extends RuntimeException {
+    public UsernameOrEmailHasAlreadyExistsException(String message) {
+        super(message);
+    }
+}
