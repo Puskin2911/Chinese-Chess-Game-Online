a8ad663abbd75b5c5cdef97ac80638b26a86d29f
Puskin2911 <nguyenthehop2000@gmail.com>
2020-11-05 22:59:54 +0700

    First Setup for React

---
+++ game-ui/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+import Board from "./components/Board/Board";
+import Header from "./components/Header/Header";
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Board/>
+        </div>
+    );
+}
+
+export default App;
---
+++ game-ui/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <nav className="navbar navbar-dark bg-primary">
+            <div className="row col-12 d-flex justify-content-center text-white">
+                <span className="h3">Register</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
