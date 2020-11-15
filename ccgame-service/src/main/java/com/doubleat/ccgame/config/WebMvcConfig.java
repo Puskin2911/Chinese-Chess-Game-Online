aac4e8e3ef71cd9c88a573d69b78588e8eb5ccc9
Hop Nguyen <hop.nguyen@eastgate-software.com>
2020-11-15 23:14:02 +0700

    WebMVC config for Swagger-UI

--- ccgame-service/src/main/java/com/doubleat/ccgame/config/WebMvcConfig.java
+++ ccgame-service/src/main/java/com/doubleat/ccgame/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.doubleat.ccgame.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
