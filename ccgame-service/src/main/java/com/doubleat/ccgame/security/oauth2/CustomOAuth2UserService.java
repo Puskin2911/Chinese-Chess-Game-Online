04a60302145bd65095650eab172b1f3d73b02657
Hop Nguyen <hop.nguyen@eastgate-software.com>
2020-11-18 22:41:38 +0700

    Update OAuth2CustomService

--- ccgame-service/src/main/java/com/doubleat/ccgame/security/oauth2/CustomOAuth2UserService.java
+++ ccgame-service/src/main/java/com/doubleat/ccgame/security/oauth2/CustomOAuth2UserService.java
@@ -69,16 +69,18 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private User registerNewUser(OAuth2UserRequest oAuth2UserRequest, OAuth2UserInfo oAuth2UserInfo) {
         User user = new User();
 
-        user.setProvider(AuthProvider.valueOf(oAuth2UserRequest.getClientRegistration().getRegistrationId()));
+        String authProviderString = oAuth2UserRequest.getClientRegistration().getRegistrationId();
+        user.setProvider(AuthProvider.valueOf(authProviderString.toUpperCase()));
         user.setProviderId(oAuth2UserInfo.getId());
         user.setUsername(oAuth2UserInfo.getUsername());
-//        user.setEmail(oAuth2UserInfo.getEmail());
+        user.setEmail(oAuth2UserInfo.getEmail());
         user.setImageUrl(oAuth2UserInfo.getImageUrl());
+        user.setElo(1200);
+        user.setPassHashed("password");
         return userRepository.save(user);
     }
 
     private User updateExistingUser(User existingUser, OAuth2UserInfo oAuth2UserInfo) {
-//        existingUser.setName(oAuth2UserInfo.getName());
         existingUser.setImageUrl(oAuth2UserInfo.getImageUrl());
         return userRepository.save(existingUser);
     }
