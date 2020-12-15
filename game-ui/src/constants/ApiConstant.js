9ea1252f6b1178a4c89423de1ac8c39a91e3c78e
Hop Nguyen <hop.nguyen@eastgate-software.com>
2020-12-15 23:11:33 +0700

    Update url back-end service

--- game-ui/src/constants/ApiConstant.js
+++ game-ui/src/constants/ApiConstant.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'https://127.0.0.1:8080';
+// const API_BASE_URL = 'https://127.0.0.1:8080';
+const API_BASE_URL = 'https://ec2-13-212-212-232.ap-southeast-1.compute.amazonaws.com:8080';
 const ACCESS_TOKEN = 'accessToken';
 
 const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
