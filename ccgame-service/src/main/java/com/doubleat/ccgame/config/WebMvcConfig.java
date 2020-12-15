c85e4cd591a10970e25a3256aa71f6886104107b
Hop Nguyen <hop.nguyen@eastgate-software.com>
2020-12-15 23:17:05 +0700

    Add allowedOrigins

--- ccgame-service/src/main/java/com/doubleat/ccgame/config/WebMvcConfig.java
+++ ccgame-service/src/main/java/com/doubleat/ccgame/config/WebMvcConfig.java
@@ -9,10 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
+    private String[] allowedOrigins = { "https://localhost:3000",
+            "http://localhost:3000",
+            "https://ec2-13-212-212-232.ap-southeast-1.compute.amazonaws.com:8080",
+            "http://ec2-13-212-212-232.ap-southeast-1.compute.amazonaws.com:8080" };
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:3000")
+                .allowedOrigins(allowedOrigins)
                 .allowCredentials(true);
     }
 
