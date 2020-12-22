29d2c9ff532c43d68c9f7c2f04fb48b812d06689
Hop Nguyen <hop.nguyen@eastgate-software.com>
2020-12-23 02:04:58 +0700

    Completed Horse

--- game-ui/src/game/piece/Horse.js
+++ game-ui/src/game/piece/Horse.js
@@ -6,7 +6,34 @@ export default class Horse extends Piece {
     }
 
     isValidMove(board, from, to) {
-        return super.isValidMove(board, from, to);
+        const xFrom = from.x + "";
+        const yFrom = from.y + "";
+        const xTo = to.x + "";
+        const yTo = to.y + "";
+
+        const pieceAtTo = board[xTo][yTo];
+
+        if (Math.abs(xFrom - xTo) === 1 && yFrom - yTo === 2 && board[xFrom][yFrom - 1] == null) {
+            if (pieceAtTo == null) return true;
+            return pieceAtTo.isRed !== this.isRed;
+        }
+
+        if (Math.abs(xFrom - xTo) === 1 && yFrom - yTo === -2 && board[xFrom][yFrom - 1 + 2] == null) {
+            if (pieceAtTo == null) return true;
+            return pieceAtTo.isRed !== this.isRed;
+        }
+
+        if (xFrom - xTo === 2 && Math.abs(yFrom - yTo) === 1 && board[xFrom - 1][yFrom] == null) {
+            if (pieceAtTo == null) return true;
+            return pieceAtTo.isRed !== this.isRed;
+        }
+
+        if (xFrom - xTo === -2 && Math.abs(yFrom - yTo) === 1 && board[xFrom - 1 + 2][yFrom] == null) {
+            if (pieceAtTo == null) return true;
+            return pieceAtTo.isRed !== this.isRed;
+        }
+
+        return false;
     }
 
 }
