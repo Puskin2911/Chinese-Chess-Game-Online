34ef5d68f103c1575939046b1e338f69a78ff1fb
An Nguyen <annguyenvan.2k@gmail.com>
2020-12-24 21:51:17 +0700

    complete Cannon server-side

--- ccgame-service/src/main/java/com/doubleat/ccgame/game/piece/Cannon.java
+++ ccgame-service/src/main/java/com/doubleat/ccgame/game/piece/Cannon.java
@@ -10,6 +10,27 @@ public class Cannon extends Piece {
 
     @Override
     public boolean isValidMove(Board board, Position from, Position to) {
-        return true;
+        int xFrom = from.getX();
+        int yFrom = from.getY() ;
+        int xTo = to.getX() ;
+        int yTo = to.getY() ;
+
+        Piece pieceAtTo = board.getPieces()[xTo][yTo];
+
+        if (xFrom == xTo) {
+            if (PieceUtils.getPieceBetweenHorizontal(board, xFrom, yFrom, yTo) == 0 && pieceAtTo == null) return true;
+            if (PieceUtils.getPieceBetweenHorizontal(board, xFrom, yFrom, yTo) == 1 && pieceAtTo != null)
+                return pieceAtTo.isRed != this.isRed;
+
+        }
+        if (yFrom == yTo) {
+            if (PieceUtils.getPieceBetweenVertical(board, xFrom, xTo, yFrom) == 0 && pieceAtTo == null) return true;
+            if (PieceUtils.getPieceBetweenVertical(board, xFrom, xTo, yFrom) == 1 && pieceAtTo != null)
+                return pieceAtTo.isRed != this.isRed;
+        }
+
+        return false;
+
     }
+
 }
