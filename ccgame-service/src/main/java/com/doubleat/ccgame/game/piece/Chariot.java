4d75266895fd9e8345100b029ecfb51b9f14ef7c
An Nguyen <annguyenvan.2k@gmail.com>
2020-12-24 21:53:32 +0700

    complete Chariot server-side

--- ccgame-service/src/main/java/com/doubleat/ccgame/game/piece/Chariot.java
+++ ccgame-service/src/main/java/com/doubleat/ccgame/game/piece/Chariot.java
@@ -10,6 +10,22 @@ public class Chariot extends Piece {
 
     @Override
     public boolean isValidMove(Board board, Position from, Position to) {
-        return true;
+        int xFrom = from.getX();
+        int yFrom = from.getY();
+        int xTo = to.getX();
+        int yTo = to.getY();
+
+        Piece pieceAtTo = board.getPieces()[xTo][yTo];
+
+        if (xFrom == xTo && PieceUtils.getPieceBetweenHorizontal(board, xFrom, yFrom, yTo) == 0) {
+            if (pieceAtTo == null) return true;
+            return pieceAtTo.isRed != this.isRed;
+        }
+        if (yFrom == yTo && PieceUtils.getPieceBetweenVertical(board, xFrom, xTo, yFrom) == 0) {
+            if (pieceAtTo == null) return true;
+            return pieceAtTo.isRed != this.isRed;
+        }
+
+        return false;
     }
 }
