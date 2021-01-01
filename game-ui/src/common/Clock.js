6c3d38deb5943ae197bbd59f74aabf0895537934
An Nguyen <nguyenvanan_t63@hus.edu.vn>
2021-01-02 00:32:40 +0700

    Update clock

--- game-ui/src/common/Clock.js
+++ game-ui/src/common/Clock.js
@@ -9,7 +9,13 @@ export default class Clock extends React.Component {
         if (!this.props.isEnable) return null;
         return (
             <div className="my-2">
-                Clock here
+                <div className="text-center">
+                    <span className="p-1 bg-white rounded">
+                        <code>
+                            Time: 00:00
+                        </code>
+                    </span>
+                </div>
             </div>
         );
     }
